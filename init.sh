c1eb3420b7d248a67f634d465b719d2dd678dcb6
ulquiorra <cion.aang@gmail.com>
2025-07-02 00:14:06 +0000

    Updated docker

--- init.sh
+++ init.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 set -e
+set -x  # ← show all commands being executed
 
 echo "▶ Starting placeholder..."
+mkdir -p ./stream/cam
+chmod -R 777 ./stream
+
 docker compose run --rm -T placeholder
 
 echo "▶ Starting full stack..."
