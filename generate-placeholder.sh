e3b41a324dc7cdeaeb9adfe67bf80736b3c3e21b
ulquiorra <cion.aang@gmail.com>
2025-07-02 01:37:56 +0000

    Updated docker

--- generate-placeholder.sh
+++ generate-placeholder.sh
@@ -7,9 +7,12 @@ rm -rf ./stream/cam/*
 
 echo "▶ Generating placeholder HLS stream from video..."
 
-ffmpeg -y -nostdin -re -i placeholder.mp4 \
+ffmpeg -y -re -i placeholder.mp4 \
   -c:v libx264 -c:a aac -preset veryfast \
-  -f hls -hls_time 2 -hls_list_size 5 -hls_flags delete_segments \
+  -f hls \
+  -hls_time 1 \                      # shorter segments (1s)
+  -hls_list_size 5 \
+  -hls_flags independent_segments \
   ./stream/cam/index.m3u8
 
 echo "✅ Placeholder stream ready."
